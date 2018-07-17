c3db26c15555a7f24d7f5d52447410abde379a40
Vinicius Costa <vinicius.costa@opah.com.br>
2018-07-17 15:37:08 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express"); //back end - node
+// import express from 'express'; //front end - react
+
+const app = express(); // represents route handlers
+
+app.get("/greeting", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+// process.env.PORT -> Server inject envivolments 
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
