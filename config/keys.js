311be71afb9579e792baeccc12139cc2f8cc186d
Vinicius Costa <vinicius.costa@opah.com.br>
2018-07-18 18:55:19 -0300

    set env

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 //keys.js - figure out what set of credentials to return
 if (process.env.PORT === "production") {
-    module.exports = require('./prod');
+    
+    module.exports = require('./dev');
 } else {
     module.exports = require('./dev');
 }
