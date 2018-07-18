10fdff82141fb27dc96d913e91c3593d056f23ca
Vinicius Costa <vinicius.costa@opah.com.br>
2018-07-18 19:20:01 -0300

    add proxy callback auth Google

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,6 @@ module.exports = {
     googleClienteID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    googleRedirectURI: 'https://dashboard.heroku.com'
 };
--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.use(
     {
       clientID: keys.googleClienteID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     async (acessToken, refreshToken, profile, done) => {
       const user = await User.findOne({ googleId: profile.id });
